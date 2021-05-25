fd344cb4a8349f523f0d0c3cd9fd85702012d08b
Allison Vacanti <alliepiper16@gmail.com>
2021-05-25 13:18:16 -0400

    [Do not commit] reduce test case for CDP error

--- testing/cuda/adjacent_difference.cu
+++ testing/cuda/adjacent_difference.cu
@@ -1,9 +1,33 @@
-#include <unittest/unittest.h>
+#include <cub/detail/ptx_dispatch_impl.cuh>
 #include <thrust/adjacent_difference.h>
-#include <thrust/execution_policy.h>
-#include <thrust/device_malloc.h>
 #include <thrust/device_free.h>
+#include <thrust/device_malloc.h>
+#include <thrust/execution_policy.h>
+#include <unittest/unittest.h>
 
+#if 1
+
+template<typename ExecutionPolicy, typename Iterator1, typename Iterator2>
+__global__ void repro_kernel(ExecutionPolicy exec, Iterator1 first, Iterator1 last, Iterator2 result)
+{
+  thrust::adjacent_difference(exec, first, last, result);
+}
+
+template <typename T>
+void Test(const std::size_t n)
+{
+  auto exec = thrust::device;
+  thrust::device_vector<T> d_input(n);
+  thrust::device_vector<T> d_output(n);
+  repro_kernel<<<1,1>>>(exec, d_input.begin(), d_input.end(), d_output.begin());
+  {
+    cudaError_t const err = cudaDeviceSynchronize();
+    ASSERT_EQUAL(cudaSuccess, err);
+  }
+}
+DECLARE_VARIABLE_UNITTEST(Test);
+
+#else
 
 template<typename ExecutionPolicy, typename Iterator1, typename Iterator2>
 __global__ void adjacent_difference_kernel(ExecutionPolicy exec, Iterator1 first, Iterator1 last, Iterator2 result)
@@ -22,19 +46,21 @@ __global__ void adjacent_difference_kernel(ExecutionPolicy exec, Iterator1 first
 template<typename T, typename ExecutionPolicy>
 void TestAdjacentDifferenceDevice(ExecutionPolicy exec, const size_t n)
 {
-  thrust::host_vector<T>   h_input = unittest::random_samples<T>(n);
-  thrust::device_vector<T> d_input = h_input;
+//  thrust::host_vector<T>   h_input = unittest::random_samples<T>(n);
+//  thrust::device_vector<T> d_input = h_input;
+  thrust::host_vector<T>   d_input(n);// = unittest::random_samples<T>(n);
 
-  thrust::host_vector<T>   h_output(n);
+//  thrust::host_vector<T>   h_output(n);
   thrust::device_vector<T> d_output(n);
 
-  thrust::adjacent_difference(h_input.begin(), h_input.end(), h_output.begin());
+//  thrust::adjacent_difference(h_input.begin(), h_input.end(), h_output.begin());
   adjacent_difference_kernel<<<1,1>>>(exec, d_input.begin(), d_input.end(), d_output.begin());
   {
     cudaError_t const err = cudaDeviceSynchronize();
     ASSERT_EQUAL(cudaSuccess, err);
   }
 
+#if 0
   ASSERT_EQUAL(h_output, d_output);
 
   thrust::adjacent_difference(h_input.begin(), h_input.end(), h_output.begin(), thrust::plus<T>());
@@ -56,8 +82,10 @@ void TestAdjacentDifferenceDevice(ExecutionPolicy exec, const size_t n)
 
   ASSERT_EQUAL(h_input, h_output); //computed previously
   ASSERT_EQUAL(d_input, d_output); //computed previously
+#endif
 }
 
+#if 0
 
 template<typename T>
 void TestAdjacentDifferenceDeviceSeq(const size_t n)
@@ -66,6 +94,7 @@ void TestAdjacentDifferenceDeviceSeq(const size_t n)
 }
 DECLARE_VARIABLE_UNITTEST(TestAdjacentDifferenceDeviceSeq);
 
+#endif
 
 template<typename T>
 void TestAdjacentDifferenceDeviceDevice(const size_t n)
@@ -74,6 +103,7 @@ void TestAdjacentDifferenceDeviceDevice(const size_t n)
 }
 DECLARE_VARIABLE_UNITTEST(TestAdjacentDifferenceDeviceDevice);
 
+#if 0
 
 void TestAdjacentDifferenceCudaStreams()
 {
@@ -144,3 +174,6 @@ void TestAdjacentDifferenceWithBigIndexes()
     TestAdjacentDifferenceWithBigIndexesHelper(33);
 }
 DECLARE_UNITTEST(TestAdjacentDifferenceWithBigIndexes);
+
+#endif
+#endif
